fe088ba20f08a4f63a4944e32d2348c10f179200
Teylan <grantsrockets@yahoo.com>
2019-10-22 18:09:20 -0700

    Refactored

--- app/src/main/java/edu/cascadia/mobile/apps/arraylistplay/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/arraylistplay/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.cascadia.mobile.apps.arraylistplay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -13,13 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //TODO: Refactor for RecyclerView
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_recyclerview);
 
         //Data stored in a String[]
         String[] myDataArray = new String[] {"Java", "Kotlin", "Swift", "Objective C"};
 
         //TODO: Refactor for RecyclerView
         //Create an adapter
+        RecyclerView.Adapter<StringArrayAdapter.ViewHolder> Adapter = new StringArrayAdapter(myDataArray);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
